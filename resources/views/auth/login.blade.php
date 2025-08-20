21c9095c2fc6c7ec55b03d572f064e94d07fcdf2
Leatpt <lea.kibur@tptlive.ee>
2025-08-20 20:11:23 +0300

    Changing Login View Style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,30 +1,22 @@
-<x-layout>
-    <x-slot:heading>
-        Logi Sisse
-    </x-slot:heading>
-
-    <form method="POST" action="/login">
+<x-layouts.app>
+    <form method="POST" action="/login" class="max-w-sm mx-auto">
         @csrf
 
-        <div class="space-y-12">
-            <div class="border-b border-gray-900/10 pb-12">
-                <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+        <div class="mb-5 mt-12 bg-white flex items-center justify-center rounded-xl border-2 border-gray-700 border-dotted">
+            <div class="border-b border-gray-900/10 pb-12 w-full max-w-md p-6">
+                <div class="grid grid-cols-1 gap-y-8">
                     <x-form-field>
-                        <x-form-label for="email">Kasutaja</x-form-label>
-
-                        <div class="mt-2">
+                        <x-form-label for="username">Kasutaja</x-form-label>
+                        <div class="mt-2 bg-lime-50">
                             <x-form-input name="username" id="username" type="text" :value="old('username')" required />
-
                             <x-form-error name="username" />
                         </div>
                     </x-form-field>
 
                     <x-form-field>
                         <x-form-label for="password">Parool</x-form-label>
-
-                        <div class="mt-2">
+                        <div class="mt-2 bg-lime-50">
                             <x-form-input name="password" id="password" type="password" required />
-
                             <x-form-error name="password" />
                         </div>
                     </x-form-field>
@@ -32,9 +24,10 @@
             </div>
         </div>
 
+
         <div class="mt-6 flex items-center justify-end gap-x-6">
             <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Tühista</a>
             <x-form-button>Logi Sisse</x-form-button>
         </div>
     </form>
-</x-layout>
+</x-layouts.app>

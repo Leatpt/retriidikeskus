9f606f44c727e98f404b001e6f591d80086335ea
Leatpt <lea.kibur@tptlive.ee>
2025-09-12 14:31:13 +0300

    Fixing Create Event Modal scaling issue with adding too many dates to see the submit button

--- resources/views/livewire/events/create.blade.php
+++ resources/views/livewire/events/create.blade.php
@@ -2,7 +2,7 @@
     <div class="fixed z-50 inset-0">
         <div class="flex justify-center items-center min-h-screen px-4">
             <div class="z-40 fixed inset-0 bg-emerald-600 opacity-50" wire:click="$set('showModal', false)"></div>
-            <div class="z-50 w-full max-w-3xl bg-white rounded-lg shadow-lg p-6" wire:click.stop>
+            <div class="z-50 w-full items-start max-h-[95vh] max-w-3xl bg-white rounded-lg shadow-lg p-6 overflow-y-auto" wire:click.stop>
                 <form class="w-full" wire:submit="save">
                     @csrf
 

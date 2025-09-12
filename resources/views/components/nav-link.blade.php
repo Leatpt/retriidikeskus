afb95edee550a101d9d19834a12359c24e88e2ec
Leatpt <lea.kibur@tptlive.ee>
2025-09-12 14:01:36 +0300

    Adjusting Mobile NavBar Styling

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,4 @@
 @props(['active' => false])
 
-<a class="{{ $active ? 'bg-lime-200 text-gray-900' : 'text-gray-800 hover:bg-lime-300 hover:text-black'}} rounded-md px-3 py-2 text-sm font-medium"
+<a class="{{ $active ? 'bg-lime-200 text-gray-900' : 'text-gray-800 hover:bg-lime-300 hover:text-black'}} rounded-md px-3 py-2 text-sm font-medium -rotate-45 sm:rotate-none "
    aria-current="{{ $active ? 'page' : 'false' }}" {{ $attributes }}>{{ $slot }}</a>
